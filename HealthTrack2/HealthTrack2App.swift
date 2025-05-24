62390c8b41ba905ea68f952c09f988c06ebf5f69
Swasti Sundar Pradhan <swastipradhan044@gmail.com>
2025-05-24 18:16:09 +0530

    Initial Commit

---
+++ HealthTrack2/HealthTrack2App.swift
@@ -0,0 +1,17 @@
+//
+//  HealthTrack2App.swift
+//  HealthTrack2
+//
+//  Created by Swasti Sundar Pradhan on 24/05/25.
+//
+
+import SwiftUI
+
+@main
+struct HealthTrack2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ HealthTrack2Tests/HealthTrack2Tests.swift
@@ -0,0 +1,17 @@
+//
+//  HealthTrack2Tests.swift
+//  HealthTrack2Tests
+//
+//  Created by Swasti Sundar Pradhan on 24/05/25.
+//
+
+import Testing
+@testable import HealthTrack2
+
+struct HealthTrack2Tests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
